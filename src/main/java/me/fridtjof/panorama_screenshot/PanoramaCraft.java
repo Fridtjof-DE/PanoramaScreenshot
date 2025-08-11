952c093855c3aee82b00c4c9abdded8073f9c4a9
PlayboyX312 <sapogov24@gmail.com>
2025-08-11 17:56:37 +0500

    Update to MC 1.21.6-8

--- src/main/java/me/fridtjof/panorama_screenshot/PanoramaCraft.java
+++ src/main/java/me/fridtjof/panorama_screenshot/PanoramaCraft.java
@@ -33,7 +33,7 @@ public class PanoramaCraft implements ModInitializer {
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (panoramaKeyBinding.wasPressed()) {
 
-				client.takePanorama(GAME_DIR, 1024, 1024);
+				client.takePanorama(GAME_DIR);
 
 
 				Text panoramaTakenText = Text.literal(PANORAMA_NAMES).formatted(Formatting.UNDERLINE).styled((style) -> {
